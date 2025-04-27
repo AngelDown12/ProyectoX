b9364421bd2c836c836627f989ed6f11ba507288
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-27 06:25:58 -0500

    Create welsticker.js

---
+++ plugins/Grupos/welsticker.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch'
+
+let handler = m => m
+
+handler.before = async function (m, { conn, groupMetadata, isBotAdmin }) {
+  if (!m.messageStubType || !m.isGroup) return
+
+  // URL del sticker para la despedida
+  const STICKER_URL = 'https://files.catbox.moe/g3hyc2.webp'
+
+  // Solo si el mensaje es de tipo "salida de usuario"
+  if (m.messageStubType == 28) {
+    // Retrasar 2 segundos antes de enviar el sticker
+    setTimeout(async () => {
+      // Obtener el sticker desde la URL
+      let sticker = await (await fetch(STICKER_URL)).buffer()
+
+      // Enviar el sticker
+      await conn.sendMessage(m.chat, { 
+        sticker: sticker  // Enviar el sticker
+      })
+    }, 2000)  // Retraso de 2 segundos (puedes ajustar el tiempo)
+  }
+}
+
+export default handler
