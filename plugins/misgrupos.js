f75e7361895a29a79d5dfb66740c686a8e1a7c18
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-27 00:39:27 -0500

    Update misgrupos.js

--- plugins/misgrupos.js
+++ plugins/misgrupos.js
@@ -1,39 +1,25 @@
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, usedPrefix }) => {
-let pp = 'https://telegra.ph/file/635b82df8d7abb4792eab.jpg'
-//const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-let user = global.db.data.users[m.sender]
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-try {
-pp = await conn.getProfilePicture(who)         //pp = await conn.getProfilePicture(who)
-} catch (e) {
+import moment from 'moment-timezone'
+import PhoneNum from 'awesome-phonenumber'
 
-} finally {
-let { name, limit, lastclaim, registered, regTime, age } = global.db.data.users[who]
-//let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let mentionedJid = [who]
-let username = conn.getName(who)
-let prem = global.prems.includes(who.split`@`[0])
-let sn = createHash('md5').update(who).digest('hex')
-let str = `┏━━°❀❬ *𝙋𝙀𝙍𝙁𝙄𝙇* ❭❀°━━┓
-┃ *🔥𝙉𝙤𝙢𝙗𝙧𝙚🔥 :* ${name}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *✨𝙉𝙪𝙢𝙚𝙧𝙤✨ :* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *🔰𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨🔰 :* wa.me/${who.split`@`[0]}${registered ?'\n┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┃ 𝙀𝙙𝙖𝙙 ' + age + ' *años*' : ''}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *💎𝙇𝙞𝙢𝙞𝙩𝙚𝙨💎 :* *${limit}* 𝙙𝙚 𝙪𝙨𝙤𝙨
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *❇️𝙍𝙚𝙜𝙞𝙨𝙩𝙧𝙖𝙙𝙤 :* ${user.registered === true ? '✅' : '❌ _#verificar_'}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *❇️𝙋𝙧𝙚𝙢𝙞𝙪𝙢 :* ${user.premiumTime > 0 ? '✅' : '❌ _#pase premium_'}
-┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *🔰 Mi estado:* ${typeof user.miestado !== 'string' ? '_#miestado || Estado no asignado_' : '_Me siento ' + user.miestado + '_'}
-┗━━━━━━━━━━━━━━`.trim()
-conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})}}
-handler.help = ['profile [@user]']
-handler.tags = ['xp']
-handler.command = /^perfil|profile?$/i
+let regionNames = new Intl.DisplayNames(['en'], { type: 'region' })
+
+let handler = async (m, { conn, text, usedPrefix, command: cmd }) => {
+	let num = m.quoted?.sender || m.mentionedJid?.[0] || text
+	if (!num) throw `*Ejemplo*: ${usedPrefix + cmd} @tag 50492280729`
+	num = num.replace(/\D/g, '') + '@s.whatsapp.net'
+	if (!(await conn.onWhatsApp(num))[0]?.exists) throw 'Este usuario no existe, asegurese de escribir bien el numero.'
+	let img = await conn.profilePictureUrl(num, 'image').catch(_ => './src/avatar_contact.png')
+	let bio = await conn.fetchStatus(num).catch(_ => { })
+	let name = await conn.getName(num)
+	let business = await conn.getBusinessProfile(num)
+	let format = PhoneNum(`+${num.split('@')[0]}`)
+	let country = regionNames.of(format.getRegionCode('international'))
+	let wea = `> •*WhatsApp Stalking🍁*\n\n*Pais :* ${country.toUpperCase()}\n*Nombre :* ${name ? name : '-'}\n*Formato:* ${format.getNumber('international')}\n*Url:* wa.me/${num.split('@')[0]}\n*Tag :* @${num.split('@')[0]}\n*Bio :* ${bio?.status || '-'}\n*Bio Uptade:* ${bio?.setAt ? moment(bio.setAt.toDateString()).locale('id').format('LL') : '-'}\n\n${business ? `*Business Info 🫐*\n*BusinessId:* ${business.wid}\n*Website:* ${business.website ? business.website : '-'}\n*Email:* ${business.email ? business.email : '-'}\n*Categoria:* ${business.category}\n*Direccion:* ${business.address ? business.address : '-'}\n\*Zona Horaria:* ${business.business_hours.timezone ? business.business_hours.timezone : '-'}\n*Descripcion*: ${business.description ? business.description : '-'}` : '> •*Cuenta De WhatsApp*'}`
+	img ? await conn.sendMessage(m.chat, { image: { url: img }, caption: wea, mentions: [num] }, { quoted: m }) : m.reply(wea)
+}
+
+handler.help = ['wastalk *<numero>*']
+handler.tags = ['tools']
+handler.command = /^(wa|whatsapp)stalk|perfil$/i
+
 export default handler
