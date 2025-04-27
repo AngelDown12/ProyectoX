dd5adc60f25795cfaf9096cf2df9f629b45b6ffe
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-27 02:42:20 -0500

    Update setbyestickers.js

--- plugins/setbyestickers.js
+++ plugins/setbyestickers.js
@@ -2,19 +2,19 @@ import fetch from 'node-fetch'
 
 let handler = m => m
 
-handler.before = async function (m, { conn, groupMetadata }) {
+handler.before = async function (m, { conn, groupMetadata, isBotAdmin }) {
   if (!m.messageStubType || !m.isGroup) return
 
   // URL del sticker para la despedida
   const STICKER_URL = 'https://files.catbox.moe/g3hyc2.webp'
 
   // Solo si el mensaje es de tipo "salida de usuario"
-  if (m.messageStubType == 28 && this.user.jid != global.conn.user.jid) {
+  if (m.messageStubType == 28) {
     // Retrasar 2 segundos antes de enviar el sticker
     setTimeout(async () => {
       // Obtener el sticker desde la URL
       let sticker = await (await fetch(STICKER_URL)).buffer()
-      
+
       // Enviar el sticker
       await conn.sendMessage(m.chat, { 
         sticker: sticker  // Enviar el sticker
