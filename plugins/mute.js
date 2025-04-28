47b77d687b6e5d1165448a1d17d768c63deca4ad
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-27 20:37:27 -0500

    Update mute.js

--- plugins/mute.js
+++ plugins/mute.js
@@ -1,16 +1,16 @@
 let mutedUsers = new Set()
 
 let handler = async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin, text }) => {
-    if (!isBotAdmin) return m.reply('𝘕𝘦𝘤𝘦𝘴𝘪𝘵𝘢 𝘴𝘦𝘳 𝘢𝘥𝘮𝘪𝘯.');
+    if (!isBotAdmin) return m.reply('𝘕𝘦𝘤𝘦𝘴𝘪𝘵𝘢 𝘴𝘦𝘳 �𝘢𝘥𝘮𝘪𝘯.');
     if (!isAdmin) return m.reply('> 𝘌𝘴𝘵𝘦 𝘤𝘰𝘮𝘢𝘯𝘥𝘰 𝘴𝘰𝘭𝘰 𝘭𝘰 𝘶𝘴𝘢𝘯 𝘢𝘥𝘮𝘪𝘯𝘴.');
 
-    let user = m.mentionedJid?.[0] || (m.quoted ? m.quoted.sender : null);
+    let user = m.mentionedJid?.[0] || (m.quoted ? m.quoted.sender : text.match(/^(\d+)/)?.[0] + '@s.whatsapp.net');
 
     if (!user) {
-        return m.reply(`> 𝘔𝘦𝘯𝘤𝘪𝘰𝘯𝘢 𝘰 𝘳𝘦𝘴𝘱𝘰𝘯𝘥𝘦 𝘢 𝘶𝘯 𝘶𝘴𝘶𝘢𝘳𝘪𝘰.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: ${usedPrefix + command} @usuario`);
+        return m.reply(`> 𝘔𝘦𝘯𝘤𝘪𝘰𝘯𝘢 𝘰 𝘳𝘦𝘴𝘱𝘰𝘯𝘥𝘦 𝘢 𝘶𝘯 �𝘴𝘶𝘢𝘳𝘪𝘰.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: ${usedPrefix + command} @usuario`);
     }
 
-    if (/^\.?mute2$/i.test(m.text)) {
+    if (/^\.?mute2$/i.test(command)) {
         mutedUsers.add(user);
         await conn.sendMessage(m.chat, {
             text: `🔇 𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘮𝘶𝘵𝘦𝘢𝘥𝘰: @${user.split('@')[0]}`,
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin, text }
         }, { quoted: m });
     }
 
-    if (/^\.?unmute2$/i.test(m.text)) {
+    if (/^\.?unmute2$/i.test(command)) {
         mutedUsers.delete(user);
         await conn.sendMessage(m.chat, {
             text: `✔️ 𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘥𝘦𝘴𝘮𝘶𝘵𝘦𝘢𝘥𝘰: @${user.split('@')[0]}`,
@@ -37,15 +37,11 @@ handler.before = async (m, { conn }) => {
     }
 }
 
-handler.help = ['mute2', 'unmute2'];
+handler.help = ['mute2 @usuario', 'unmute2 @usuario'];
 handler.tags = ['group'];
-handler.customPrefix = /^(\.?)(mute2|unmute2)$/i;
-handler.command = new RegExp; 
+handler.command = /^(mute2|unmute2)$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 
 export default handler;
-
-
-
