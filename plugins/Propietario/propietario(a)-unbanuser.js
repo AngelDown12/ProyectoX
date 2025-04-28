459f5cacf96f449ce9f02e29fba48666f1b7434f
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-28 18:35:36 -0500

    Update propietario(a)-unbanuser.js

--- plugins/Propietario/propietario(a)-unbanuser.js
+++ plugins/Propietario/propietario(a)-unbanuser.js
@@ -1,4 +1,4 @@
-/*let handler = async (m, { conn, text, command, usedPrefix }) => {
+let handler = async (m, { conn, text, command, usedPrefix }) => {
 if (!text) throw `${mg}𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝙀 𝘼 𝘼𝙇𝙂𝙐𝙄𝙀𝙉 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} @tag*\n\n𝙏𝘼𝙂 𝙎𝙊𝙈𝙀𝙊𝙉𝙀 𝙏𝙊 𝙐𝙉𝘽𝘼𝙉\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} @tag*`
 let who
 if (m.isGroup) who = m.mentionedJid[0]
@@ -15,4 +15,4 @@ handler.group = true
 handler.admin = true 
 handler.owner = true
 export default handler
-*/
+
