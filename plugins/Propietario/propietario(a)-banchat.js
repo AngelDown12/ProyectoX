f514d93ba96d6dd9d96c97229b556779f178a844
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-28 17:32:08 -0500

    Update propietario(a)-banchat.js

--- plugins/Propietario/propietario(a)-banchat.js
+++ plugins/Propietario/propietario(a)-banchat.js
@@ -1,16 +1,4 @@
-/*let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🥳\n𝘼𝙃𝙊𝙍𝘼 𝙎𝙄 𝙀𝙎𝙏𝙊𝙔 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀.\n\n𝙏𝙃𝙄𝙎 𝘾𝙃𝘼𝙏 𝙒𝘼𝙎 𝙐𝙉𝘽𝘼𝙉 😌\n𝙉𝙊𝙒 𝙄 𝘼𝙈 𝘼𝙑𝘼𝙄𝙇𝘼𝘽𝙇𝙀.`)
-}
-handler.help = ['unbanchat2']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.rowner = true
-handler.exp = 50
-
-export default handler*/
-
-//PARA GRUPOS
+/*
 
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = true
@@ -22,3 +10,4 @@ handler.command = /^banchat|banchat2$/i
 handler.botAdmin = true
 handler.rowner = true
 export default handler
+*/
