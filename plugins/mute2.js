a5cb6a90fa4cbb3b4a2ce41951ef08a63b1d4e15
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-27 20:13:40 -0500

    Update mute2.js

--- plugins/mute2.js
+++ plugins/mute2.js
@@ -2,7 +2,7 @@ let mutedUsers = new Set()
 
 let handler = async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin, text }) => {
     if (!isBotAdmin) return m.reply('𝘕𝘦𝘤𝘦𝘴𝘪𝘵𝘢 𝘴𝘦𝘳 𝘢𝘥𝘮𝘪𝘯.');
-    if (!isAdmin) return m.reply('> 𝘌𝘴𝘵𝘦 𝘤𝘰𝘮𝘢𝘯𝘥𝘰 𝘴𝘰𝘭𝘰 𝘭𝘰 𝘶𝘴𝘢𝘯 𝘢𝘥𝘮𝘪𝘯𝘴.');
+    if (!isAdmin) return m.reply('> 𝘌𝘴𝘵𝘦 𝘤𝘰𝘮𝘢𝘯𝘥𝘰 𝘴𝘰𝘭𝘰 𝘭𝘰 𝘶𝘴𝘢𝘯 �𝘢𝘥𝘮𝘪𝘯𝘴.');
 
     let user = m.mentionedJid?.[0] || (m.quoted ? m.quoted.sender : null);
 
@@ -10,15 +10,15 @@ let handler = async (m, { conn, usedPrefix, command, isAdmin, isBotAdmin, text }
         return m.reply(`> 𝘔𝘦𝘯𝘤𝘪𝘰𝘯𝘢 𝘰 𝘳𝘦𝘴𝘱𝘰𝘯𝘥𝘦 𝘢 𝘶𝘯 𝘶𝘴𝘶𝘢𝘳𝘪𝘰.\n\n𝘌𝘫𝘦𝘮𝘱𝘭𝘰: ${usedPrefix + command} @usuario`);
     }
 
-    if (/^\.?mute2$/i.test(m.text)) {
+    if (/^\.?mute$/i.test(m.text)) {
         mutedUsers.add(user);
         await conn.sendMessage(m.chat, {
-            text: `🔇 𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘮𝘶𝘵𝘦𝘢𝘥𝘰: @${user.split('@')[0]}`,
+            text: `🔇 �𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘮𝘶𝘵𝘦𝘢𝘥𝘰: @${user.split('@')[0]}`,
             mentions: [user]
         }, { quoted: m });
     }
 
-    if (/^\.?unmute2$/i.test(m.text)) {
+    if (/^\.?unmute$/i.test(m.text)) {
         mutedUsers.delete(user);
         await conn.sendMessage(m.chat, {
             text: `✔️ 𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘥𝘦𝘴𝘮𝘶𝘵𝘦𝘢𝘥𝘰: @${user.split('@')[0]}`,
@@ -37,9 +37,9 @@ handler.before = async (m, { conn }) => {
     }
 }
 
-handler.help = ['mute2', 'unmute2'];
+handler.help = ['mute', 'unmute'];
 handler.tags = ['group'];
-handler.customPrefix = /^(\.?)(mute2|unmute2)$/i;
+handler.customPrefix = /^(\.?)(mute|unmute)$/i;
 handler.command = new RegExp; 
 handler.group = true;
 handler.admin = true;
