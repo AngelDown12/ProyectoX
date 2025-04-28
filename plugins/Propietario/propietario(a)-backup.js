461e64ab585a01cc31e1976a9b0a5324e54f2dbf
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-28 17:32:34 -0500

    Update propietario(a)-backup.js

--- plugins/Propietario/propietario(a)-backup.js
+++ plugins/Propietario/propietario(a)-backup.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+/*import fs from 'fs'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
@@ -20,3 +20,4 @@ handler.command = /^(backup|respaldo|copia)$/i
 handler.owner = true
 
 export default handler
+*/
