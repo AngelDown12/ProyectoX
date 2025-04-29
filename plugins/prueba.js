860c8743978a6d7826d0470883bc82cb635fb85b
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-29 12:31:36 -0500

    Update prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,12 +1,7 @@
-import translate from '@vitalets/google-translate-api';
-import axios from 'axios';
-import fetch from 'node-fetch';
-
-const handler = (m) => m;
-
 handler.before = async (m, { conn }) => {
   const chat = global.db.data.chats[m.chat];
   if (chat.simi) {
+    if (m.text?.startsWith(global.prefix)) return; // ⛔ Ignora comandos
     if (/^.*false|disable|(turn)?off|0/i.test(m.text)) return;
 
     let textodem = m.text;
@@ -17,7 +12,7 @@ handler.before = async (m, { conn }) => {
 
     try {
       const username = `${conn.getName(m.sender)}`;
-      const basePrompt = `Tu nombre es EliteBotGlobal y parece haber sido creado por BotBarboza-Ai. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explosiones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`;
+      const basePrompt = `Tu nombre es BarbozaBot y parece haber sido creado por BotBarboza-Ai. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, te encanta aprender y sobre todo las explosiones. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`;
 
       const prompt = `${basePrompt}. Responde lo siguiente: ${textodem}`;
 
@@ -32,22 +27,3 @@ handler.before = async (m, { conn }) => {
   }
   return true;
 };
-
-export default handler;
-
-// Función para interactuar con tu API
-async function callBarbozaAPI(query, username, prompt) {
-  try {
-    const response = await axios.post("https://Luminai.my.id", {
-      content: query,
-      user: username,
-      prompt: prompt,
-      webSearchMode: false
-    });
-
-    return response.data.result?.trim() || '💛 Lo siento, no pude responder eso.';
-  } catch (error) {
-    console.error('💛 Error al obtener respuesta de Luminai:', error);
-    return '💛 Hubo un error al procesar tu solicitud. Intenta de nuevo más tarde.';
-  }
-}
