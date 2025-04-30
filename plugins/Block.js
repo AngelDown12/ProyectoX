3bee46dbf6a2e64ca88ef6a18f26a29edb6cf06e
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-30 13:27:37 -0500

    Update Block.js

--- plugins/Block.js
+++ plugins/Block.js
@@ -1,20 +1,27 @@
-export async function before(m, {isOwner, isROwner}) {
+const NUMERO_EXCLUIDO = '573243951424@s.whatsapp.net'; // Bot principal
+
+export async function before(m, { isOwner, isROwner }) {
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
   if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
+
   let bot = global.db.data.settings[this.user.jid] || {};
+
+  // No bloquear si el remitente es el número excluido
+  if (m.sender === NUMERO_EXCLUIDO) return !0;
 
   if (bot.antiPrivate && !isOwner && !isROwner) {
     await m.reply(
       `» 𝗘𝗦𝗧𝗔 𝗣𝗥𝗢𝗛𝗜𝗕𝗜𝗗𝗢 𝗘𝗦𝗖𝗥𝗜𝗕𝗜𝗥𝗟𝗘 𝗔𝗟 𝗕𝗢𝗧 𝗔𝗟 𝗣𝗥𝗜𝗩𝗔𝗗𝗢, 𝗦𝗘𝗥𝗔́𝗦 𝗕𝗟𝗢𝗤𝗨𝗘𝗔𝗗𝗢/𝗔 .
 ﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘
-» 𝗦𝗜 𝗡𝗘𝗖𝗘𝗦𝗜𝗧𝗔𝗦 𝗨𝗡 𝗕𝗢𝗧 𝗢 𝗧𝗜𝗘𝗡𝗘𝗦 𝗔𝗟𝗚𝗨́𝗡 𝗜𝗡𝗖𝗢𝗡𝗩𝗘𝗡𝗜𝗘𝗡𝗧𝗘 𝗖𝗢𝗡𝗧𝗔́𝗖𝗧𝗔𝗧𝗘 𝗖𝗢𝗡 𝗠𝗜 𝗖𝗥𝗘𝗔𝗗𝗢𝗥.
-» Wa.me/593993370003 ${md}`,
+» 𝗦𝗜 𝗡𝗘𝗖𝗘𝗦𝗜𝗧𝗔𝗦 𝗨𝗡 𝗕𝗢𝗧 𝗢 𝗧𝗜𝗘𝗡𝗘𝗦 𝗔𝗟𝗚Ú𝗡 𝗜𝗡𝗖𝗢𝗡𝗩𝗘𝗡𝗜𝗘𝗡𝗧𝗘 𝗖𝗢𝗡𝗧Á𝗖𝗧𝗔𝗧𝗘 𝗖𝗢𝗡 𝗠𝗜 𝗖𝗥𝗘𝗔𝗗𝗢𝗥.
+» Wa.me/593993370003`,
       false,
-      {mentions: [m.sender]}
+      { mentions: [m.sender] }
     );
-    await this.updateBlockStatus(m.chat, "block");
+    await this.updateBlockStatus(m.chat, 'block');
   }
+
   return !1;
 }
