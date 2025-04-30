ff6039d285327c884778b73dbdd84e01b7f7e783
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-30 00:16:47 -0500

    Update .n.js

--- plugins/.n.js
+++ plugins/.n.js
@@ -1,54 +1,99 @@
-const handler = async (m, { conn, text, participants, args }) => {
+import {generateWAMessageFromContent} from "@whiskeysockets/baileys";
+let handler = async (m, {conn, text, participants}) => {
   try {
-    if (args.length >= 1) {
-      text = args.join(" ")
-    } else if (m.quoted && m.quoted.text) {
-      text = m.quoted.text
-    } else return
+    let users = participants.map((u) => conn.decodeJid(u.id));
+    let q = m.quoted ? m.quoted : m || m.text || m.sender;
+    let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender;
+    let msg = conn.cMod(
+      m.chat,
+      generateWAMessageFromContent(
+        m.chat,
+        {[m.quoted ? q.mtype : "extendedTextMessage"]: m.quoted ? c.message[q.mtype] : {text: "" || c}},
+        {quoted: m, userJid: conn.user.id}
+      ),
+      text || q.text,
+      conn.user.jid,
+      {mentions: users}
+    );
+    await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
+  } catch {
+    /**
+[ By @NeKosmic || https://github.com/NeKosmic/ ]
+**/
 
-    const users = participants.map((u) => conn.decodeJid(u.id))
-    await conn.sendMessage(m.chat, {
-      text: text,
-      contextInfo: { mentionedJid: users }
-    }, { quoted: m })
-
-  } catch (e) {
-    const users = participants.map((u) => conn.decodeJid(u.id))
-    const quoted = m.quoted ? m.quoted : m
-    const mime = (quoted.msg || quoted).mimetype || ''
-    const isMedia = /image|video/.test(mime)
-    const htextos = text ? text : '📣📣📣'
-
-    if (isMedia && quoted.mtype === 'imageMessage') {
-      const mediax = await quoted.download?.()
-      await conn.sendMessage(m.chat, {
-        image: mediax,
-        caption: htextos,
-        contextInfo: { mentionedJid: users }
-      }, { quoted: m })
-
-    } else if (isMedia && quoted.mtype === 'videoMessage') {
-      const mediax = await quoted.download?.()
-      await conn.sendMessage(m.chat, {
-        video: mediax,
-        caption: htextos,
-        mimetype: 'video/mp4',
-        contextInfo: { mentionedJid: users }
-      }, { quoted: m })
-
+    let users = participants.map((u) => conn.decodeJid(u.id));
+    let quoted = m.quoted ? m.quoted : m;
+    let mime = (quoted.msg || quoted).mimetype || "";
+    let isMedia = /image|video|sticker|audio/.test(mime);
+    let more = String.fromCharCode(8206);
+    let masss = more.repeat(850);
+    let htextos = `${text ? text : "*Hola :D*"}`;
+    if (isMedia && quoted.mtype === "imageMessage" && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(
+        m.chat,
+        {
+          image: mediax,
+          mentions: users,
+          caption: htextos,
+          mentions: users,
+        },
+        {}
+      );
+    } else if (isMedia && quoted.mtype === "videoMessage" && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(
+        m.chat,
+        {
+          video: mediax,
+          mentions: users,
+          mimetype: "video/mp4",
+          caption: htextos,
+          mentions: users,
+        },
+        {}
+      );
+    } else if (isMedia && quoted.mtype === "audioMessage" && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(
+        m.chat,
+        {
+          audio: mediax,
+          mentions: users,
+          mimetype: "audio/mp4",
+          fileName: `Hidetag.mp3`,
+          mentions: users,
+        },
+        {}
+      );
+    } else if (isMedia && quoted.mtype === "stickerMessage" && htextos) {
+      var mediax = await quoted.download?.();
+      conn.sendMessage(
+        m.chat,
+        {
+          sticker: mediax,
+          mentions: users,
+          mentions: users,
+        },
+        {}
+      );
     } else {
-      await conn.sendMessage(m.chat, {
-        text: htextos,
-        contextInfo: { mentionedJid: users }
-      }, { quoted: m })
+      await conn.relayMessage(
+        m.chat,
+        {
+          extendedTextMessage: {
+            text: `${masss}\n${htextos}\n`,
+            ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen4, sourceUrl: "https://github.com/DIEGO-OFC/DORRAT-BOT-MD"}}},
+          },
+        },
+        {}
+      );
     }
   }
-}
+};
 
-handler.command = /^(hidetag|notificar|notify|aviso|avisar|noti|avisa)$/i
-handler.group = true
-handler.admin = true
-handler.register = true
-handler.botAdmin = true
+handler.command = /^(hidetag|notificar|notify)$/i;
+handler.group = true;
+handler.admin = true;
 
-export default handler
+export default handler;
