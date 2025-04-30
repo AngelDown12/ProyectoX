bdc2a316e031e3fb9205b13cfc2f78f3f9e9feb9
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-30 12:11:18 -0500

    Update Block.js

--- plugins/Block.js
+++ plugins/Block.js
@@ -1,9 +1,16 @@
 // BLOQUEA EN EL NÚMERO PRINCIPAL. EN LOS SUBBOTS DEJA DE RESPONDER.
 const BOT_PRINCIPAL = '593986304370@s.whatsapp.net'; // Reemplaza con el número real del bot principal
 
-const handler = async (m, { conn, isOwner }) => {
-  if (conn.user.jid !== BOT_PRINCIPAL) return; // Solo ejecuta si es el bot principal
-  if (!m.isGroup && !isOwner) {
+export async function before(m, { isOwner, isROwner }) {
+  if (this.user?.jid !== BOT_PRINCIPAL) return !0; // Solo ejecuta si es el bot principal
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes("PIEDRA") || m.text.includes("PAPEL") || m.text.includes("TIJERA")) return !0;
+
+  let bot = global.db.data.settings[this.user.jid] || {};
+
+  if (bot.antiPrivate && !isOwner && !isROwner) {
     const userMention = '@' + m.sender.split('@')[0];
     const fecha = new Date().toLocaleDateString('es-EC', {
       weekday: 'long',
@@ -13,18 +20,15 @@ const handler = async (m, { conn, isOwner }) => {
     });
 
     await m.reply(
-      `Hola ${userMention}\n\nEstá prohibido escribirme al privado, por ende serás bloqueado.\n\nFuiste bloqueado\n(${fecha})`,
-      null,
+      `Hola ${userMention}\n\nEstá prohibido escribirme al privado, por ende serás bloqueado.\n\nFuiste bloqueado\n(${fecha})\n\n` +
+      `» Si necesitas un bot o tienes algún inconveniente, contáctate con mi creador:\n` +
+      `» wa.me/593993370003`,
+      false,
       { mentions: [m.sender] }
     );
 
-    await conn.updateBlockStatus(m.sender, 'block'); // Esta línea es la función original
+    await this.updateBlockStatus(m.chat, "block");
   }
-};
 
-handler.customPrefix = /.*/;
-handler.command = new RegExp; // la dejamos si es parte de la estructura del bot
-handler.private = true;
-handler.owner = false;
-
-export default handler;
+  return !1; // Previene que otros handlers actúen
+}
