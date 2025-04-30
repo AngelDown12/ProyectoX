c5f3fdc30568d00358f2958bc7241e9263bc2381
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-04-30 13:43:43 -0500

    Create Blo.js

---
+++ plugins/Blo.js
@@ -0,0 +1,30 @@
+const handler = async (m, { conn, args, command, isOwner, isROwner }) => {
+  if (!isOwner && !isROwner) {
+    return m.reply('Este comando es solo para el owner.');
+  }
+
+  if (!args[0]) {
+    return m.reply(`Ejemplo:\n.blo 593993370003\n.unblo 593993370003`);
+  }
+
+  const numero = args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net';
+
+  try {
+    if (command === 'blo') {
+      await conn.updateBlockStatus(numero, 'block');
+      m.reply(`Usuario ${args[0]} bloqueado correctamente.`);
+    } else if (command === 'unblo') {
+      await conn.updateBlockStatus(numero, 'unblock');
+      m.reply(`Usuario ${args[0]} desbloqueado correctamente.`);
+    }
+  } catch (e) {
+    m.reply('Ocurrió un error al intentar bloquear/desbloquear.');
+  }
+};
+
+handler.command = ['blo', 'unblo'];
+handler.owner = true;
+handler.group = false;
+handler.private = true;
+
+export default handler;
