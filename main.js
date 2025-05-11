968935d531c78a0e92b3e1a6d0199509a7c625b3
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-05-11 11:44:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 //import './settings.js'
-import { setupMaster, fork } from 'cluster'
+//import { setupMaster, fork } from 'cluster'
 import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts'
 import {createRequire} from 'module'
