036a5e90eb1de1a28364700235222d278549f805
desconocido <129682184+desconocido1515@users.noreply.github.com>
2025-05-15 21:32:01 -0500

    Update _owner-check.js

--- plugins/_owner-check.js
+++ plugins/_owner-check.js
@@ -1,6 +1,11 @@
-// plugins/zzz-debug-owner.js
-let handler = async (m, { conn }) => {
-  m.reply('Tu JID es: ' + m.sender)
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const isOwner = global.owner.some(([id]) => id === m.sender)
+  if (!isOwner) {
+    return m.reply('No eres mi dueño. No puedes usar este comando.')
+  }
+  return m.reply('¡Perfecto! El bot te reconoce como OWNER.')
 }
-handler.command = /^myjid$/i
+
+handler.command = ['testowner']
+handler.owner = false  // No usamos la propiedad, hacemos la validación manual
 export default handler
