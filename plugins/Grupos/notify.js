b131ddd836efb649d85939baeb316d9dff6d08e0
AngelDown12 <angeldown12xz@gmail.com>
2025-07-23 15:55:09 -0600

    Actualizar notify.js

--- plugins/Grupos/notify.js
+++ plugins/Grupos/notify.js
@@ -47,7 +47,7 @@ const handler = async (m, { conn, text, participants }) => {
     console.error('Error en el comando hidetag:', e);
     const usersFallback = participants.map(u => conn.decodeJid(u.id));
     await conn.sendMessage(m.chat, {
-      text: (text || '') + '\nㅤㅤㅤㅤㅤㅤㅤㅤ𝔈𝔩𝔦𝔱𝔢 𝔅𝔬𝔱 𝔊𝔩𝔬𝔟𝔞𝔩',
+      text: (text || '') + '\nㅤㅤㅤㅤㅤㅤㅤㅤ𝘽𝙐𝙐 𝘽𝙊𝙏 𝙊𝙁𝙄𝘾𝙄𝘼𝙇',
       mentions: usersFallback
     }, { quoted: m });
   }
