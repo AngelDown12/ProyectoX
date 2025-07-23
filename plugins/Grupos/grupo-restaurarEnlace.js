0b4989e586e3ad1ff4d707696aef64024584c306
AngelDown12 <angeldown12xz@gmail.com>
2025-07-23 01:40:18 -0600

    Actualizar grupo-restaurarEnlace.js

--- plugins/Grupos/grupo-restaurarEnlace.js
+++ plugins/Grupos/grupo-restaurarEnlace.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/5k7vwl.jpg' 
 
 let res = await conn.groupRevokeInvite(m.chat)
 conn.reply(m.chat, lenguajeGB.smsRestGp(), + '\n\n*https://chat.whatsapp.com/' + res + '*', fkontak, m)}
