0104b3d5b9572b61b7a33a1dde9c9d95593d5703
AngelDown12 <angeldown12xz@gmail.com>
2025-07-23 02:00:41 -0600

    Actualizar casos.js

--- plugins/Juegos/casos.js
+++ plugins/Juegos/casos.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Todavia hay un juego sin terminar!', conn.tekateki[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync("./src/game/casos.json", 'utf8'))
+    let tekateki = JSON.parse(fs.readFileSync("https://files.catbox.moe/5k7vwl.jpg", 'utf8'))
     let json = tekateki[Math.floor(Math.random() * tekateki.length)]
     let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
