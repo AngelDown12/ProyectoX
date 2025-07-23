444d547e38bd6fe1d39ae4f55d8c90643737b56c
AngelDown12 <angeldown12xz@gmail.com>
2025-07-23 01:46:02 -0600

    Actualizar rpg-unreg.js

--- plugins/Grupos/rpg-unreg.js
+++ plugins/Grupos/rpg-unreg.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*\n*By: EliteBotGlobal*`, m)
+return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*\n*By: 𝙗𝙪𝙪 𝙗𝙤𝙩 𝙤𝙛𝙞𝙘𝙞𝙖𝙡*`, m)
 
 }
 handler.help = ['unreg']
