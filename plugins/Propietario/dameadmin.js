70bd11c1ab8f15c55f9ff195eef1ab4f796f12be
AngelDown12 <angeldown12xz@gmail.com>
2025-07-23 02:20:29 -0600

    Actualizar dameadmin.js

--- plugins/Propietario/dameadmin.js
+++ plugins/Propietario/dameadmin.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, isAdmin }) => {
   if (isAdmin) throw `*Usted ya es administrador de este grupo creador.*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
-handler.command = /^dameadmin|tenerpoder$/i
+handler.command = /^autoadmin|tenerpoder$/i
 handler.rowner = true
 handler.botAdmin = true
 export default handler
